2c1f6ec06e06647d2f4cab9f8b0963a7c841126a
Joshua Hawxwell <m@hawx.me>
2014-12-17 15:59:55 +0000

    Fix edwn drawing with whitespace on left

--- recipes/edwn.go
+++ recipes/edwn.go
@@ -9,41 +9,41 @@ import (
 )
 
 const (
-	TOP_BORDER = 0.1
-	SIDE_BORDER = 0.1
+	TOP_BORDER    = 0.1
+	SIDE_BORDER   = 0.1
 	BOTTOM_BORDER = 0.3
 )
 
 func Edwn(in image.Image) image.Image {
 	// Crop:
-
-	in = crop.Square(in, -1, utils.Centre)
+	in = crop.Square(in, -1, utils.Right)
 	rect := in.Bounds()
 
 	// Calculate border widths:
-
-	topBorder    := int(float64(rect.Dy()) * TOP_BORDER)
-	sideBorder   := int(float64(rect.Dx()) * SIDE_BORDER)
+	topBorder := int(float64(rect.Dy()) * TOP_BORDER)
+	sideBorder := int(float64(rect.Dx()) * SIDE_BORDER)
 	bottomBorder := int(float64(rect.Dy()) * BOTTOM_BORDER)
 
-	fullWidth  := rect.Dx() + 2 * sideBorder + 2
+	fullWidth := rect.Dx() + 2*sideBorder + 2
 	fullHeight := rect.Dy() + topBorder + bottomBorder + 2
 
-	// Put white border round image
-	// Put 1px black border round
+	// Bounds now start off (0, 0) since the image has been cropped, so need to
+	// correct for this when calculating the image size and remove the margin
+	imageSize := image.Rect(sideBorder+1-rect.Min.X, topBorder+1-rect.Min.Y, sideBorder+1+rect.Dx(), topBorder+1+rect.Dy())
 
-	imageSize := image.Rect(sideBorder+1, topBorder+1, sideBorder+1 + rect.Dx(), topBorder+1 + rect.Dy())
-	whiteSize := image.Rect(1, 1, fullWidth - 1, fullHeight - 1)
+	// Put white border round image
+	whiteSize := image.Rect(1, 1, fullWidth-1, fullHeight-1)
+
+	// Put 1px black border round
 	blackSize := image.Rect(0, 0, fullWidth, fullHeight)
 
-	white     := image.NewUniform(color.White)
-	black     := image.NewUniform(color.Black)
+	white := image.NewUniform(color.White)
+	black := image.NewUniform(color.Black)
 
 	final := image.NewRGBA(blackSize)
-
 	draw.Draw(final, blackSize, black, image.Pt(0, 0), draw.Src)
 	draw.Draw(final, whiteSize, white, image.Pt(0, 0), draw.Src)
-  draw.Draw(final, imageSize, in, image.Pt(0, 0), draw.Src)
+	draw.Draw(final, imageSize, in, image.Pt(0, 0), draw.Src)
 
 	return final
 }
